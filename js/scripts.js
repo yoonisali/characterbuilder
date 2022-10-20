f09074b26bf61bf6856c68c7a8c99a402afbca79
yoonisali <yoonismahamoudali@gmail.com>
2022-10-20 12:37:22 -0700

    add branching and css styling
    Co-authored-by: Ryan Krueger <thegckid@gmail.com>
    Co-authored-by: Bodie Wood <bodiewood@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,57 @@
-// Buisness Logic
+function handleWarrior(event) {
+  event.preventDefault();
 
-function warrior() {
+  let shaqWarrior = document.querySelector("div#shaqWarrior");
+  let shaqMagic = document.querySelector("div#shaqMagic");
+  let shaqArchery = document.querySelector("div#shaqArchery");
+  let averageWarrior = document.querySelector("div#averageWarrior");
+  let averageMagic = document.querySelector("div#averageMagic");
+  let averageArchery = document.querySelector("div#averageArchery");
+  let tinyWarrior = document.querySelector("div#tinyWarrior");
+  let tinyMagic = document.querySelector("div#tinyMagic");
+  let tinyArchery = document.querySelector("div#tinyArchery");
 
 
-  document.getElementById("reveal2").setAttribute('class', 'hidden');
-  const name = document.getElementById('name').value;
-  const height = document.getElementById("height").value;
-  const class1 = document.querySelector("input[name='operator']:checked").value;
-  const dob = document.getElementById("dob").value;
-  const color = document.getElementById("color").value;
 
+  let height = document.getElementById("height").value;
+  const operator = document.querySelector("input[name='operator']:checked").value;
 
-  // UI Logic
-  let result;
-  if (height === 'shaq' && class1 === 'warrior') {
-    result = document.removeAttribute("hidden");
-  } else if (height === 'shaq' && class1 === 'magic') {
-    result = document.removeAttribute('hidden');
-  } else if (height === 'shaq' && class1 === 'archery') {
-    result = document.removeAttribute('hidden');
+  shaqWarrior.setAttribute("class", "hidden");
+  shaqMagic.setAttribute("class", "hidden");
+  shaqArchery.setAttribute("class", "hidden");
+  averageWarrior.setAttribute("class", "hidden");
+  averageMagic.setAttribute("class", "hidden");
+  averageArchery.setAttribute("class", "hidden");
+  tinyWarrior.setAttribute("class", "hidden");
+  tinyMagic.setAttribute("class", "hidden");
+  tinyArchery.setAttribute("class", "hidden");
+
+  if (height === 'shaq' && operator === 'warrior') {
+    shaqWarrior.removeAttribute('class', 'hidden');
+  } else if (height === 'shaq' && operator === 'magic') {
+    shaqMagic.removeAttribute('class', 'hidden');
+  } else if (height === 'shaq' && operator === 'archery') {
+    shaqArchery.removeAttribute('class', 'hidden');
+  } else if (height === 'average' && operator === 'warrior') {
+    averageWarrior.removeAttribute('class', 'hidden');
+  } else if (height === 'average' && operator === 'magic') {
+    averageMagic.removeAttribute('class', 'hidden');
+  } else if (height === 'average' && operator === 'archery') {
+    averageArchery.removeAttribute('class', 'hidden');
+  } else if (height === 'tiny' && operator === 'warrior') {
+    tinyWarrior.removeAttribute('class', 'hidden');
+  } else if (height === 'tiny' && operator === 'magic') {
+    tinyMagic.removeAttribute('class', 'hidden');
+  } else if (height === 'tiny' && operator === 'archery') {
+    tinyArchery.removeAttribute('class', 'hidden');
   }
+  
 
 
+  // the code to get and process form values will go here!
 }
-
-
-
-
-
-
 
 window.addEventListener("load", function () {
   const form = document.getElementById("character");
-  form.addEventListener("submit", function(e){
-    e.preventDefault(); 
-    warrior();
-  });
+  form.addEventListener("submit", handleWarrior);
 });
